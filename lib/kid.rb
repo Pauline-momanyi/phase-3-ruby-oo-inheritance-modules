9bad30e15ab78cde0d70d99859effe18f30bb89d
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-06-11 03:25:20 +0300

    commit tree 2dc6f1975eeba2c0ffe6b5c78678b8efde3cbcec

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,18 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
+require_relative "./dance_module"
+require_relative "./meta_dancing_module"
+require_relative "./fancy_dance"
+
+class Kid
+  #   include Dance #use all methods in the module as instance methods
+  #   extend MetaDancing #extend creates a class method
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+# angelina = Kid.new("Angelina")
+# puts "#{angelina.name} says: #{angelina.twirl}"
